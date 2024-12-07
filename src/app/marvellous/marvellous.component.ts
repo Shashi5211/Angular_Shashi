77689900c1067b01f744d25be93842468fb87fa8
Shashi5211 <shashikantyelikar24@gmail.com>
2024-12-07 16:13:23 +1100

    Add files via upload

---
+++ src/app/marvellous/marvellous.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+
+
+
+
+@Component({
+  selector: 'app-marvellous',
+  standalone: true,
+  imports: [],
+  templateUrl: './marvellous.component.html',
+  styleUrl: './marvellous.component.css'
+})
+export class MarvellousComponent 
+{
+  public First=""
+  public Last=""
+  public Phone=""
+  public Address=""
+  public mail=""
+  public Password=""
+  
+ 
+  
+
+ 
+}
